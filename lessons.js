66a3f680b8dace8e4b46c897b7c2c80f9b79c590
nafisarc <nafisanidhi05@gmail.com>
2025-10-17 15:44:42 +0400

    Add images, lessons.js, and style.css

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+{
+    id: 1001,
+    title: "Dance",
+    description: "Description",
+    price: 2000,
+    image: "images/dancelesson.png",
+    availableInventory: 5,
+    rating: 3
+},    
+{
+    id: 1002,
+    title: "Music",
+    description: "Description",
+    price: 2000,
+    image: "images/musiclesson.png",
+    availableInventory: 5,
+    rating: 3
+},  
+{
+    id: 1003,
+    title: "Debate",
+    description: "Description",
+    price: 2000,
+    image: "images/debatelesson.png",
+    availableInventory: 5,
+    rating: 3
+},  
+{
+    id: 1004,
+    title: "Swimming",
+    description: "Description",
+    price: 2000,
+    image: "images/swimminglessons.png",
+    availableInventory: 5,
+    rating: 3
+},  
+{
+    id: 1005,
+    title: "Karate",
+    description: "Description",
+    price: 2000,
+    image: "images/karatelesson.png",
+    availableInventory: 5,
+    rating: 3
+},  
+{
+    id: 1006,
+    title: "Literature",
+    description: "Description",
+    price: 2000,
+    image: "images/literaturelesson.png",
+    availableInventory: 5,
+    rating: 3
+},  
+{
+    id: 1007,
+    title: "Math",
+    description: "Description",
+    price: 2000,
+    image: "images/mathlesson.png",
+    availableInventory: 5,
+    rating: 3
+},  
+{
+    id: 1008,
+    title: "Arts and Crafts",
+    description: "Description",
+    price: 2000,
+    image: "images/artsncraftslesson.png",
+    availableInventory: 5,
+    rating: 3
+},  
+{
+    id: 1009,
+    title: "Gymnastics",
+    description: "Description",
+    price: 2000,
+    image: "images/gymnasticslesson.png",
+    availableInventory: 5,
+    rating: 3
+},  
+{
+    id: 1010,
+    title: "Football",
+    description: "Description",
+    price: 2000,
+    image: "images/footballlesson.png",
+    availableInventory: 5,
+    rating: 3
+},  
+]
